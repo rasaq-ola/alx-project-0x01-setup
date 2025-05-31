35d02dd6f9c1d42ecdc9d25bb3b788e632b65d4f
Rasaq-ola <rasaqola196@gmail.com>
2025-05-31 04:37:15 +0100

    correcting my UserCard.tsx

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,7 +1,15 @@
 import { UserProps } from "@/interfaces";
 
-const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
-  return (
+const UserCard: React.FC<UserProps> = ({
+  name,
+  username,
+  email,
+  phone,
+  website,
+  company,
+  address,
+}) => {
+	return (
     <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
       <div className="mb-4">
         <h2 className="text-2xl font-semibold text-gray-800">{user.name}</h2>
